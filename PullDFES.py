086ef57a534c43453dc2a457925435561e98cdf7
Chris Raven <chris@magentium.com>
2018-12-31 19:59:16 +0800

    Fixed Menu Items and Big Update

--- PullDFES.py
+++ PullDFES.py
@@ -3,8 +3,9 @@
 
 import feedparser
 import time
+import os
 
-print('Pulling Array Information. Please wait ... \n')
+print('\n Pulling Array Information. Please wait ... \n')
 feed_incidents = feedparser.parse("https://www.emergency.wa.gov.au/data/message.rss")
 arrayIncidents = []
 for i in range(len(feed_incidents.entries)):
@@ -16,12 +17,6 @@ arrayPrescribedBurns = []
 for i in range(len(feed_prescribed_burns.entries)):
     arrayIncidents.append({"URL": feed_prescribed_burns.entries[i].link,"Title": feed_prescribed_burns.entries[i].title, "Time": feed_prescribed_burns.entries[i].published})
 print('Prescribed Burns         ' + u'\u2713')
-
-feed_fireDangerWarnings = feedparser.parse('https://www.emergency.wa.gov.au/data/message_FDR.rss')
-arrayFireDangerRatings = []
-for i in range(len(feed_fireDangerWarnings.entries)):
-    arrayFireDangerRatings.append({"URL": feed_fireDangerWarnings.entries[i].link,"Title": feed_fireDangerWarnings.entries[i].title, "Time": feed_fireDangerWarnings.entries[i].published})
-print('Fire Danger Ratings      ' + u'\u2713')
 
 feed_firebans = feedparser.parse('https://www.emergency.wa.gov.au/data/message_TFB.rss')
 arrayFirebans = []
@@ -48,22 +43,25 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def breaker(ascii):
+    '''This is a breaker that separates information based on the ASCII text of your choice'''
     print(bcolors.HEADER + (str(ascii) * 50) + bcolors.ENDC)
 
 def displayFeed(arrayInput):
-    arrayCount =0 # Set this to 0 as a Counter to start with
+    ''' Displays the Feed depending on what array is specified'''
+    arrayCount = 0 # Set this to 0 as a Counter to start with
 
     # Increment the number in ArrayCount if the items are valid, to get the number of items
     for values in range(0,len(arrayInput)):
-        if 'https://www.emergency.wa.gov.au/#map' in arrayInput[values]["URL"]:
+        if '#map' in arrayInput[values]["URL"]:
+            arrayCount +=1
+        elif '#firedangerratings' in arrayInput[values]["URL"]:
+            arrayCount +=1
+        elif '#totalfirebans' in arrayInput[values]["URL"]:
+            arrayCount +=1
+        elif 'http://www.bom.gov.au/wa/warnings' in arrayInput[values]["URL"]:
             arrayCount +=1
 
-    # If there is only 1 item in the list
-
-    if arrayCount == 0 :
-        print('There are no incidents \n')
-
-    elif arrayCount == 1 :
+    if arrayCount == 1 :
         breaker("=")
         print(bcolors.OKBLUE + "There is only one item currently" + bcolors.ENDC)
         breaker("=")
@@ -73,16 +71,86 @@ def displayFeed(arrayInput):
         print(bcolors.FAIL + "URL: " + bcolors.ENDC+  arrayInput[0]["URL"])
         print(bcolors.FAIL + "Published: " + bcolors.ENDC +  arrayInput[0]["Time"])
         breaker("=")
+        arrayCount = 0
 
     elif arrayCount > 1:
         print(bcolors.WARNING + "There are " + str(arrayCount) + " items currently" + bcolors.ENDC)
         breaker("=")
         for i in range(0,arrayCount):
-            if "https://www.emergency.wa.gov.au/#map" in arrayInput[i]["URL"]:
-                print(bcolors.FAIL + "URL: " + bcolors.ENDC+  arrayInput[i]["URL"])
                 print(bcolors.FAIL + "Title: " + bcolors.ENDC + arrayInput[i]["Title"])
+                print(bcolors.FAIL + "URL: " + bcolors.ENDC+  arrayInput[i]["URL"])
                 print(bcolors.FAIL + "Published: " + bcolors.ENDC +  arrayInput[i]["Time"])
                 breaker("-")
 
-breaker("=")
-displayFeed(arrayIncidents)
+    else:
+        print('There are no incidents \n')
+
+def clearscreen():
+    '''Clear the screen depending on what OS you're running'''
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def menu():
+    clearscreen()
+    ''' The Main Menu'''
+    print('DFES and EMERGENCY SERVICES')
+    breaker("=")
+    print('1. Incidents')
+    print('2. Prescribed Burns')
+    print('3. Firebans')
+    print('4. Weather Warnings')
+
+    print('\nPlease select an option\n')
+    userchoice = input('Please choose a menu item > ')
+    try:
+        userchoice = int(userchoice)
+    except:
+        pass
+
+    clearscreen()
+
+    if userchoice == 1:
+        incidentmenu()
+    
+    elif userchoice == 2:
+        prescribedBurnsMenu()
+
+    elif userchoice == 3:
+        firebansmenu()
+
+    elif userchoice == 4:
+        weatherwarningsWAmenu()
+            
+    else:
+        print('Invalid Option')
+        time.sleep(2)
+        menu()
+
+def incidentmenu():
+    '''Display the Incident Menu'''
+    displayFeed(arrayIncidents)
+    print('Press any key to return to the main menu')
+    input('> ')
+    menu()
+
+def prescribedBurnsMenu():
+    ''' Display the Prescribed Burns menu '''
+    displayFeed(arrayPrescribedBurns)
+    print('Press any key to return to the main menu')
+    input('> ')
+    menu()
+
+def firebansmenu():
+    ''' Display the Fire Bans menu'''
+    displayFeed(arrayFirebans)
+    print('Press any key to return to the main menu')
+    input('> ')
+    menu()
+
+def weatherwarningsWAmenu():
+    ''' Display the Weather Warnings menu'''
+    displayFeed(arrayWeatherWarningsWA)
+    print('Press any key to return to the main menu')
+    input('> ')
+    menu()
+
+menu()
